3baf8ec75da8341f851ed870dfa713cb713ea19b
César Tzoc <csartalva01@gmail.com>
2024-09-07 13:22:24 -0600

    feat: adding root role to the admin user

---
+++ src/main/resources/db/sa-root/V8__role_root_for_default_user.sql
@@ -0,0 +1,3 @@
+-- root role for admin user
+INSERT INTO public.adm_user_role (user_role_id, role_id, user_id) VALUES
+('7e75ac19-da3a-4bcc-a249-f78e5db1b26a', 'c9742ad2-842c-4279-a839-9659c9328c29', 'a97686bb-29ae-4c32-a384-624edc0858a6');
